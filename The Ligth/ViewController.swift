deba5387545c239c09180061086349b54d5723db
Mikhail Ivanov <msivanov80@mail.ru>
2023-03-02 16:22:07 +0100

    Hide status bar

--- The Ligth/ViewController.swift
+++ The Ligth/ViewController.swift
@@ -8,24 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-var isLightOn = false
+    
+    var isLightOn = true
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
         
     }
-
+    
     fileprivate func updateUI() {
-   
+        
         view.backgroundColor = isLightOn ? .white : .black
     }
     
     @IBAction func buttonPressed() {
         isLightOn.toggle()
         updateUI()
-       
+        
     }
     
 }
